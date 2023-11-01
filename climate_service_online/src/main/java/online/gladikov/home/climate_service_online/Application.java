ff6a0b96faa8334d48e4fcd3500b6ba4fd75111c
max <msgladikov@gmail.com>
2023-11-01 14:11:37 +0500

    discovery service

--- climate_service_online/src/main/java/online/gladikov/home/climate_service_online/Application.java
+++ climate_service_online/src/main/java/online/gladikov/home/climate_service_online/Application.java
@@ -2,9 +2,11 @@ package online.gladikov.home.climate_service_online;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
