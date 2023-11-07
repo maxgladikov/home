bbf88e754031f219f450730456dcaa844ef77692
max <msgladikov@gmail.com>
2023-11-07 15:44:47 +0500

    config correction

--- gateway/src/main/java/online/gladikov/home/gateway/Application.java
+++ gateway/src/main/java/online/gladikov/home/gateway/Application.java
@@ -2,11 +2,8 @@ package online.gladikov.home.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
