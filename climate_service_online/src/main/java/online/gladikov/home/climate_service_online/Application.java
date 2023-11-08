974dfb8ace2abd2b6dfbefef4e4cf52bdbf457e4
max <msgladikov@gmail.com>
2023-11-08 10:37:38 +0500

    again config

--- climate_service_online/src/main/java/online/gladikov/home/climate_service_online/Application.java
+++ climate_service_online/src/main/java/online/gladikov/home/climate_service_online/Application.java
@@ -2,7 +2,6 @@ package online.gladikov.home.climate_service_online;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
